78c9b9a8b7ebf247651e2dbe7d1b7cf2e4cf9476
Adam Raine <ad.st.raine@gmail.com>
2021-08-16 13:37:56 -0400

    legacy condition

--- lighthouse-core/runner.js
+++ lighthouse-core/runner.js
@@ -132,7 +132,9 @@ class Runner {
 
       /** @type {LH.RawIcu<LH.Result>} */
       const i18nLhr = {
-        gatherMode: artifacts.GatherContext.gatherMode,
+        // TODO(FR-COMPAT): Remove this condition when legacy runner is deprecated.
+        // If GatherContext is not collected, then we are in a non-default pass of the legacy runner.
+        gatherMode: artifacts.GatherContext ? artifacts.GatherContext.gatherMode : 'navigation',
         userAgent: artifacts.HostUserAgent,
         environment: {
           networkUserAgent: artifacts.NetworkUserAgent,
